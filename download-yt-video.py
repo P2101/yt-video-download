283d46d8408832ec715ad5a1f315852fa8638042
P2101 <98814765+P2101@users.noreply.github.com>
2023-03-18 12:08:27 +0100

    Delete download-yt-video.py

--- download-yt-video.py
+++
@@ -1,7 +0,0 @@
-import pytube # pip install pytube
-
-url = input('Enter video url: ')
-
-path = "C:/Users/Pau Pons/OneDrive/Escritorio/Pau/youtube" # the storage path of the video
-
-pytube.YouTube(url).streams.get_highest_resolution().download(path) # download the video
